2da9af745603bbe0aea2d48cd02a079e52e5062c
Ravindra Ranwala <ravindraranwala@Ravindras-MacBook-Pro.local>
2021-06-13 17:49:56 +0530

    code refactoring and improvements.

--- src/main/java/org/assessments/amazon/dp/LongestPalindromicSubstring.java
+++ src/main/java/org/assessments/amazon/dp/LongestPalindromicSubstring.java
@@ -12,7 +12,7 @@ class LongestPalindromicSubstring {
 	}
 
 	public static String longestPalindrome(String s) {
-		if(s.isEmpty())
+		if (s.isEmpty())
 			return s;
 
 		final int n = s.length();
@@ -29,14 +29,11 @@ class LongestPalindromicSubstring {
 				if (s.charAt(i) == s.charAt(j) && (len == 2 || l[i + 1][j - 1] == len - 2)) {
 					l[i][j] = len;
 					start = i;
-				} else if (l[i][j - 1] >= l[i + 1][j]) {
-					l[i][j] = l[i][j - 1];
 				} else {
-					l[i][j] = l[i + 1][j];
+					l[i][j] = Math.max(l[i][j - 1], l[i + 1][j]);
 				}
 			}
 		}
 		return s.substring(start, start + l[0][n - 1]);
 	}
-
 }
