fd4cd28aee5c501e729779c186839a133e642572
Ravindra Ranwala <ravindraranwala@Ravindras-MacBook-Pro.local>
2021-06-13 14:45:17 +0530

    code refactoring and improvements added.

--- src/main/java/org/assessments/amazon/dp/LongestPalindromicSubstring.java
+++ src/main/java/org/assessments/amazon/dp/LongestPalindromicSubstring.java
@@ -6,22 +6,21 @@ class LongestPalindromicSubstring {
 	}
 
 	public static void main(String[] args) {
-		final String s = "cbbd";
+		final String s = "aaaa";
 		final String p = longestPalindrome(s);
 		System.out.println(p);
 	}
 
 	public static String longestPalindrome(String s) {
-		if (s.isEmpty())
-			return "";
+		if(s.isEmpty())
+			return s;
 
 		final int n = s.length();
 		final int[][] l = new int[n][n];
-		final int[][] start = new int[n][n];
-
+		int start = -1;
 		for (int i = 0; i < n; i++) {
 			l[i][i] = 1;
-			start[i][i] = i;
+			start = i;
 		}
 
 		for (int len = 2; len <= n; len++) {
@@ -29,17 +28,15 @@ class LongestPalindromicSubstring {
 				final int j = i + len - 1;
 				if (s.charAt(i) == s.charAt(j) && (len == 2 || l[i + 1][j - 1] == len - 2)) {
 					l[i][j] = len;
-					start[i][j] = i;
+					start = i;
 				} else if (l[i][j - 1] >= l[i + 1][j]) {
 					l[i][j] = l[i][j - 1];
-					start[i][j] = start[i][j - 1];
 				} else {
 					l[i][j] = l[i + 1][j];
-					start[i][j] = start[i + 1][j];
 				}
 			}
 		}
-		return s.substring(start[0][n - 1], start[0][n - 1] + l[0][n - 1]);
+		return s.substring(start, start + l[0][n - 1]);
 	}
 
 }
