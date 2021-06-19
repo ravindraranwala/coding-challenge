95de576726f98d0c072852e8120b016f197c4150
Ravindra Ranwala <ravindraranwala@Ravindras-MacBook-Pro.local>
2021-06-19 07:10:15 +0530

    code refactoring.

--- src/main/java/org/assessments/dp/FibonacciNumber.java
+++ src/main/java/org/assessments/dp/FibonacciNumber.java
@@ -15,16 +15,14 @@ public class FibonacciNumber {
 		// trivial case.
 		if (n < 2)
 			return n;
-		final int[] fibSeries = new int[2];
-		fibSeries[0] = 0;
-		fibSeries[1] = 1;
-		for (int i = 2; i <= n; i++)
-		{
-			final int f = fibSeries[0] + fibSeries[1];
+		int beforePrev = 0;
+		int prev = 1;
+		for (int i = 2; i <= n; i++) {
+			final int tmp = beforePrev + prev;
 			// then, swap it.
-			fibSeries[0] = fibSeries[1];
-			fibSeries[1] = f;
+			beforePrev = prev;
+			prev = tmp;
 		}
-		return fibSeries[1];
+		return prev;
 	}
 }
